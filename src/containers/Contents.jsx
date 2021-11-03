4edf7f6e4ce7acae15d7aa85f34873702a3af3e2
zurdozarazaga <zurdozarazaga@gmail.com>
2021-11-03 10:20:08 -0300

    se agregaron los seervices y modificaciones

---
+++ src/containers/Contents.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Contents() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contents;
---
+++ src/containers/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>
+        <a href="/">
+          <img src="/images/logo.png" alt="logo" />
+        </a>
+      </h1>
+    </header>
+  )
+};
+
+export default Header
